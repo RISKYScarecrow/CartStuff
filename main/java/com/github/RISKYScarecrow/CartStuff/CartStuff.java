ce78e8f36fd90b4c381fc7c46bcce245021962a9
Matthew Underwood <Matthew.B.Underwood1995@gmail.com>
2014-05-20 21:05:12 +1000

    First commit

---
+++ main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
@@ -0,0 +1,39 @@
+package com.github.RISKYScarecrow.CartStuff;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+
+@Mod(modid = CartStuff.MODID, version = CartStuff.VERSION)
+
+
+public class CartStuff {
+
+	public static final String MODID = "cartstuff";
+	public static final String VERSION = "1.0.0";
+
+	
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) 
+	{
+	
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) 
+	{
+	
+	}
+
+}
