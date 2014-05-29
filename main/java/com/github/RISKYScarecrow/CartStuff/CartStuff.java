0f839f14b569f5166d1532910f0592aef1f21a77
RISKYScarecrow <Matthew.B.Underwood1995@gmail.com>
2014-05-29 14:16:30 +1000

    Empty message

--- main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
+++ main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
@@ -14,7 +14,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 public class CartStuff {
 
 	public static final String MODID = "cartstuff";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	
 	
