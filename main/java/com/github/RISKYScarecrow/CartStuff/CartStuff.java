3fde9c40ced1490bf8e9bc41503a204edd93e190
RISKYScarecrow <matthew.b.underwood1995@gmail.com>
2014-05-31 21:42:46 +1000

    Item ID's are gone...

--- main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
+++ main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
@@ -1,39 +1,36 @@
 package com.github.RISKYScarecrow.CartStuff;
 
-import net.minecraft.init.Blocks;
+import com.github.RISKYScarecrow.CartStuff.Initalisation.ItemDeclaration;
+import com.github.RISKYScarecrow.CartStuff.Initalisation.Recipes;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CartStuff.MODID, version = CartStuff.VERSION)
+public class CartStuff
+{
 
-
-public class CartStuff {
-
-	public static final String MODID = "cartstuff";
+	public static final String MODID = "CartStuff";
 	public static final String VERSION = "1.0.1";
 
-	
-	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		
-	}
-	
-	@EventHandler
-	public void init(FMLInitializationEvent event) 
-	{
-	
-	}
-	
-	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) 
-	{
-	
+		ItemDeclaration.addItems();
 	}
 
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		Recipes.add();
+	}
+
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+
+	}
 }
