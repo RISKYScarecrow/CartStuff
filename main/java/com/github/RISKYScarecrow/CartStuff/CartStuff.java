bc10f79afc52fc250d2a0b3cdb1989f18d086181
RISKYScarecrow <matthew.b.underwood1995@gmail.com>
2014-06-04 10:13:29 +1000

    All done for chris

--- main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
+++ main/java/com/github/RISKYScarecrow/CartStuff/CartStuff.java
@@ -3,44 +3,44 @@ package com.github.RISKYScarecrow.CartStuff;
 import net.minecraft.item.Item;
 
 import com.github.RISKYScarecrow.CartStuff.Initalisation.Initalisation;
-import com.github.RISKYScarecrow.CartStuff.modItems.Spokes;
-import com.github.RISKYScarecrow.CartStuff.modItems.Wheel;
+import com.github.RISKYScarecrow.CartStuff.Proxy.CommonProxy;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CartStuff.MODID, version = CartStuff.VERSION)
-
-public class CartStuff {
+public class CartStuff
+{
 
 	public static final String MODID = "CartStuff";
 	public static final String VERSION = "1.0.1";
 
 	public static Item spokes;
 	public static Item wheel;
-	
-	// @SidedProxy(clientSide =
-	// "com.github.RISKYScarecrow.CartStuff.Proxy.ClientProxy", serverSide =
-	// "com.github.RISKYScarecrow.CartStuff.Proxy.CommonProxy")
-	// public static CommonProxy proxy;
 
+	@SidedProxy(clientSide = "com.github.RISKYScarecrow.CartStuff.Proxy.ClientProxy", serverSide = "com.github.RISKYScarecrow.CartStuff.Proxy.CommonProxy")
+	public static CommonProxy proxy;
 
 	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		
+	public void preInit(FMLPreInitializationEvent event)
+	{
+
 		Initalisation.initalise();
 	}
 
 	@EventHandler
-	public void init(FMLInitializationEvent event) {
+	public void init(FMLInitializationEvent event)
+	{
 
 	}
 
 	@EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
+	public void postInit(FMLPostInitializationEvent event)
+	{
 
 	}
 }
